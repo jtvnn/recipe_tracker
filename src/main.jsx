41100281372c93f7548cbb410f697111c24fbe36
Jason Vann <vannjt1@icloud.com>
2025-09-07 10:46:36 -0400

    change to lux

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import 'bootswatch/dist/flatly/bootstrap.min.css';
+import 'bootswatch/dist/lux/bootstrap.min.css';
 import './index.css';
 import App from './App.jsx';
 
